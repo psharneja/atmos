6df2d72080946aedc3e4a510bd80c9c3ca098900
pr293504 <pr293504@wipro.com>
2018-07-23 21:16:02 +0530

    linting

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,42 +1,53 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import 'owfont/css/owfont-regular.css';
 
-const Details = props => (
-  <div className="row weather-row">
-    <div className="col-md-2">
-      <div className={`owf owf-${props.weather.id}`} />
-      <br />
-      <span>
-        {props.weather.main}
-      </span>
-    </div>
-    <div className="col-md-3 weather-row__date">
-      {props.date.format('MMM D')}
-      <br />
-      {props.date.format('ha')}
-    </div>
-    <div className="col-md-3">
-      <span>
-        {Math.round(props.humidity)}
-%
+const Details = ({
+  weather, date, humidity, temp,
+}) => {
+  Details.propTypes = {
+    weather: PropTypes.instanceOf(Object).isRequired,
+    date: PropTypes.string.isRequired,
+    humidity: PropTypes.string.isRequired,
+    temp: PropTypes.string.isRequired,
+  };
+  return (
+    <div className="row weather-row">
+      <div className="col-md-2">
+        <div className={`owf owf-${weather.id}`} />
         <br />
-humidity
-      </span>
-    </div>
-    <div className="col-md-3">
-      <span>
-        {Math.round(props.temp)}
-        <sup>
-o
-        </sup>
         <span>
-C
+          {weather.main}
         </span>
-      </span>
+      </div>
+      <div className="col-md-3 weather-row__date">
+        {date.format('MMM D')}
+        <br />
+        {date.format('ha')}
+      </div>
+      <div className="col-md-3">
+        <span>
+          {Math.round(humidity)}
+%
+          <br />
+humidity
+        </span>
+      </div>
+      <div className="col-md-3">
+        <span>
+          {Math.round(temp)}
+          <sup>
+o
+          </sup>
+          <span>
+C
+          </span>
+        </span>
 
-    </div>
-  </div>
-);
+      </div>
+    </div>);
+};
 
 export default Details;
